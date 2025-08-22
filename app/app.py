73d5848cd4aa96629a52ae638e37fa067f07bc5e
ap786771 <154984891+ap786771@users.noreply.github.com>
2025-08-23 02:59:02 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from app.routes import routes
+from routes import routes  # relative import
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
